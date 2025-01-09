cddb55c7e5d4740a40d9d187a155566925154789
Amaan Javed <142109083+Amaan292003@users.noreply.github.com>
2025-01-10 00:07:26 +0530

    Update page.js

--- app/invalid/page.js
+++ app/invalid/page.js
@@ -4,7 +4,7 @@ const page = () => {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
     <h1 className="text-2xl font-bold text-red-600">Invalid Razorpay Credentials</h1>
-    <p className="mt-4 text-lg">The Razorpay key or secret provided is invalid. Please try again.</p>
+    <p className="mt-4 text-lg">The Razorpay key or secret provided in dashboard is invalid. Please try again.</p>
 </div>
   )
 }
